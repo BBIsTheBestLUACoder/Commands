b19d23a03aadb7c92bf010ee65b57560a28e3090
Novoline4Roblox <125512934+StaryLOL@users.noreply.github.com>
2023-07-14 14:28:19 -0700

    Create ultrathingy.lua

---
+++ ultrathingy.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NovolineRoblox/wlsysting/main/full.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StaryLOL/Commands/main/spwhitelist.lua"))()
